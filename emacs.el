6b8f419f75a36196179414f89a2d79d928cc8127
Luke San Antonio <lukesanantonio@gmail.com>
2016-05-18 16:14:22 -0400

    Use Iosevka font

--- emacs.el
+++ emacs.el
@@ -7,12 +7,7 @@
 
 (setq inhibit-startup-screen t)
 
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:weight normal :height 97 :width normal :family "Droid Sans Mono Dotted for Powerline")))))
+(custom-set-faces '(default ((t (:family "Iosevka Term")))))
 
 ;; Initialize package.el
 (require 'package)
